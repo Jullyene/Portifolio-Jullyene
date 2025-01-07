9d3eb7b1561da161985610bbe58a1f985f399772
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2025-01-07 14:56:54 -0300

    Término da estilização do Mobile

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,39 @@
 import Banner from "@/public/components/Mobile/Banner/Banner";
 import SobreMim from "@/public/components/Mobile/SobreMim/SobreMim";
+import Projects from "@/public/components/Mobile/Projects/Projects";
 
 export default function Home() {
   return (
     <>
       <Banner/>
       <SobreMim/>
+      
+      <Projects
+      success= {true}
+      urlI= '/assets/images/png/centric.png'
+      textoAlternativo= 'Centric'
+      texto= 'Enquanto estagiava na Wecode criei o site da Centric em next.js, salientando que o design do site foi montado por um design do time.'
+      />
+
+      <Projects
+      urlI= '/assets/images/png/loucos-santos-universo-da-marca.png'
+      textoAlternativo= 'Loucos&Santos'
+      texto= 'Durante o período do Estágio também criei a página Universo da Marca Loucos&Santos'
+      />
+      
+      <Projects
+      urlI= '/assets/images/png/stihl.png'
+      textoAlternativo= 'STIHL'
+      texto= 'Durante o período do Estágio atendendo o suporte  criei a página Exclusivo loja física  da Loja STHIL com VTEX IO.'
+      />
+
+      <Projects
+      urlI= '/assets/images/png/anselmi.png'
+      textoAlternativo= 'STIHL'
+      texto= 'Durante o período de Estágio também fiz alterações nas páginas de categoria com nova estilização no breadcrumb nome da coleção e descrição da coleção., sendo possível cadastrar via Site Editor do VTEX IO um banner para a categoria.'
+      />
+
+
     </>
   );
 }
--- public/components/Mobile/Projects/Projects.jsx
+++ public/components/Mobile/Projects/Projects.jsx
@@ -1,10 +1,11 @@
-export default function Projects() {
+import styles from "./sass/projects.module.css"
+
+export default function Projects({success= false, urlI, textoAlternativo, texto}) {
     return(
-
-        //Tenho que criar um array de objetos
-        <section>
-            <h1>PROJECTS</h1>
-
+        <section className={styles.sectionProjects}>
+            {success && <h3 className={styles.titleProjects}>PROJECTS</h3>}
+            <img className={styles.image} src={urlI} alt={textoAlternativo} />
+            <p className={styles.texto}>{texto}</p>
         </section>
     );
 }
