8ed46cffebab7c1970b9aa1fb57c5081db33150e
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2025-01-25 18:10:28 -0300

    Finalizando mobile | Removi o menu porque não tinha nada pra ser adicionado nele

--- public/components/Mobile/header/Header.jsx
+++ public/components/Mobile/header/Header.jsx
@@ -1,14 +1,43 @@
-
-import Link from 'next/link'
-
-import styles from "./sass/header.module.css"
-
+import Link from 'next/link';
+import styles from "./sass/header.module.css";
+import { useState } from 'react';
 
 export default function Header() {
-    return (
+  // Criar um estado para controlar a abertura do menu
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Alternar o estado de menu aberto/fechado
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+    console.log("MenuOpen:", menuOpen);
+  };
+
+  return (
+    <>
       <section className={styles.headerMobile}>
         <h3 className={styles.title}>Portfólio</h3>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu" data-id="element-7"><line x1="4" x2="20" y1="12" y2="12"></line><line x1="4" x2="20" y1="6" y2="6"></line><line x1="4" x2="20" y1="18" y2="18"></line></svg>
+        {/* <button onClick={handleClick} className={styles.btn}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"> <line x1="4" x2="20" y1="12" y2="12"></line> <line x1="4" x2="20" y1="6" y2="6"></line> <line x1="4" x2="20" y1="18" y2="18"></line> </svg>
+        </button> */}
       </section>
-    );
-  }
+
+      {/* <section className={styles.sideMenu}>
+          {menuOpen && (
+            <nav className={styles.menu}>
+              <ul>
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="about">Sobre</Link>
+                </li>
+                <li>
+                  <Link href="projects">Projetos</Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+      </section> */}
+    </>
+  );
+}
